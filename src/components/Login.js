8e7574029f6cdb3fc4c98f727943a6d1f7d50c28
Luuchoh <luxo997@gmail.com>
2021-08-05 21:54:08 -0500

    haciendo funcionar el login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,27 +1,107 @@
-import React from 'react'
-import { Navbar } from './Navbar';
+import React, { Component } from "react";
+import axios from "axios";
+import md5 from "md5";
+import { Link } from "react-router-dom";
 
-import '../styles/Login.css';
+import { Navbar } from "./Navbar";
+import "../styles/Login.css";
 
-export const Login = () => {
+const baseUrl = "https://movies-geek.herokuapp.com/usuario";
+
+export class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      form: {
+        username: "",
+        password: "",
+      },
+    };
+  }
+
+  handleChange = async (e) => {
+    await this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
+
+  iniciarSesion = async () => {
+    await axios
+      .get(baseUrl, {
+        params: {
+          username: this.state.form.username,
+          password: md5(this.state.form.password),
+        },
+      })
+      .then((response) => {
+        return response.data;
+      })
+      .then((response) => {
+        if (response.length > 0) {
+          var respuesta = response[0];
+          alert(`Bienvenido ${respuesta.nombre} ${respuesta.apellido_paterno}`);
+          this.props.history.push("/Heroes");
+        } else {
+          alert("El usuario o la contraseña no son correctos");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  handleSutmit = (e) => {
+    e.preventDefault();
+  };
+
+  render() {
     return (
-        <div>
-        <Navbar/>
+      <div>
+        <Navbar />
         <div id="content-contacto">
-            <h1>Bienvenido</h1>
-            <form id="contacto">
-                <div className="mb-3">
-                    <label for="exampleInputEmail1" className="form-label">Email </label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-
-                </div>
-                <div className="mb-3">
-                    <label for="exampleInputPassword1" className="form-label">Contraseña</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" />
-                </div>
-                <button type="submit" className="btn btn-primary">Ingresar</button>
-            </form>
+          <h1>Bienvenido</h1>
+          <form id="contacto" onSubmit={this.handleSutmit}>
+            <div className="mb-3">
+              <label htmlFor="InputEmail1" className="form-label">
+                Email
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="InputEmail1"
+                name="username"
+                aria-describedby="emailHelp"
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="InputPassword" className="form-label">
+                Contraseña
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="InputPassword"
+                name="password"
+                onChange={this.handleChange}
+              />
+            </div>
+            <button type="submit" className="btn btn-primary"
+            onClick={this.iniciarSesion}>
+              Ingresar
+            </button>
+            <Link
+                to="/"
+                className="link mx-auto mt-4 text-white "
+            >
+                Aún no te has registrado?
+            </Link>
+          </form>
         </div>
-        </div>
-    )
+      </div>
+    );
+  }
 }
